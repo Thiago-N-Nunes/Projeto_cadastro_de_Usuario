c0e4283dcbd2fea98b5ff7585e0b42ef6318f37d
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-07-01 20:49:16 -0300

    Criando Base projeto

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+conexao = sqlite3.connect('users.db')
+cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios(
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+            nome TEXT NOT NULL,
+            idade INTEGER NOT NULL,
+            email TEXT NOT NULL
+            )""")
+
+while True:
+    nome = input('Digite o nome: ')   
+    if len(nome) == 0:
+        os.system('cls')
+        print('Cadastro finalizado!')
+        break
+    idade = int(input('Digite sua idade: '))
+    email  = input('Digite seu email: ')
+    cursor.execute("INSERT INTO usuarios(nome,idade,email) VALUES(?,?,?)", (nome,idade,email))
+
+
+conexao.commit()
