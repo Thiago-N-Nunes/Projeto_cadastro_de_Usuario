dd8a21be9aa73acd0e410ef3bc4a7c3a1739f094
THIAGO NESI NUNES <246593626+Thiago-N-Nunes@users.noreply.github.com>
2026-07-08 09:45:08 -0300

    implementando funções

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
 import main
+import os
 
-main.cadastrar()
-main.criar_id()
+print('=' * 35)
+print('BEM VINDO AO SISTEMA DE CADASTRO')
+print('=' * 35)
+escolha = int(input("""O que deseja fazer?
+1. Cadastrar
+2. Consultar
+3. Atualizar
+4. Sair\n"""))
+
+match escolha:
+    case 1:
+        main.cadastrar()
+    case 2:
+        main.consultar()
+    case 3:
+        main.alterar()
+    case 4:
+        os.system('cls')
+        print('Até a próxima!')
+
--- src/main.py
+++ src/main.py
@@ -14,29 +14,35 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios(
             )""")
 
 # Loop para cadastro de usuários, se o usuário digitar um nome vazio, o cadastro é finalizado
-while True:
-    nome = input('Digite o nome: ')   
-    if len(nome) == 0:
-        os.system('cls')
-        print('Cadastro finalizado!')
-        break
-    idade = int(input('Digite sua idade: '))
-    email  = input('Digite seu email: ')
-    cursor.execute("INSERT INTO usuarios(nome,idade,email) VALUES(?,?,?)", (nome,idade,email))
-conexao.commit()        
+def cadastrar():
+    while True:
+        nome = input('Digite o nome: ')   
+        idade = int(input('Digite sua idade: '))
+        email  = input('Digite seu email: ')
+        cursor.execute("INSERT INTO usuarios(nome,idade,email) VALUES(?,?,?)", (nome,idade,email))
+        conexao.commit()
+        escolha = input('Deseja continuar? [S/N]')
+        escolha = escolha.upper() 
+        if escolha == 'N':
+            os.system('cls')
+            print('Cadastro finalizado!')    
+            break
+       
 
-def gerador_de_id():
-    #gera um numero aleatorio de 1 a 999
-    return str(randint(0,999))
-
-def criar_id_final():
-    #gera um numero aleatorio de 1 a 999
-    nomes = cursor.execute("SELECT nome FROM usuarios")
-    nomes = nomes.fetchall()
-    nomes = list(nomes)
-    print(nomes)
-    
-    for i in range(len(nomes)):
-        id_gerado
-
+def consultar():
+    usuarios = cursor.execute("SELECT * FROM usuarios")
+    usuarios = usuarios.fetchall()
+    id, nome, idade, email = usuarios
+    print('CONSULTAR')
+    consultado = int(input("""Qual seria o interesse da consulta?
+1. NOME
+2. IDADE
+3. EMAIL\n"""))
+    match consultado:
+        case 1:
+            print('.' + nome)
+        case 2:
+            print()
+def alterar():
+    ...
     
